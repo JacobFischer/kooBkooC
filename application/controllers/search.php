eaa8317d33dcc44d6a1ea22d17f74ae22ccc5ce1
Jacob Fischer <jfischer@ubuntu-server.(none)>
2012-02-20 21:47:42 -0600

    Search Cookware basic implimentation added for the search guys to learn from

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -23,11 +23,43 @@ class Search extends CI_Controller {
         $data = array("json" => array());
         
         $data["json"] = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
-        $this->load->view('search_index', $data);
+        $this->load->view('search_json', $data);
         
         // templated view:
         //$this->template->load('template', 'welcome_message', $data);
 	}
+    
+    public function cookware()
+    {
+        // An array of cookware to return
+        $cookwareToReturn = array();
+        
+        // Build the SQL-ish query using CodeIgniters's Active Record
+        $this->db->select('*');
+        $this->db->from('Cookware');
+        
+        // Actually execute the SQL Query on the database
+        $query = $this->db->get();
+        
+        // Iterate through each result in the query and build the cookware to return
+        $i = 0;
+        foreach($query->result() as $cookware)
+        {
+            $cookwareToReturn[$i] = $cookware;
+            /*$cookwareToReturn[$i] = array
+            (
+                "ID" => $cookware->ID,
+                "Description" => $cookware->Description,
+                "Name" => $cookware->Name,
+                "ImageURL" => $cookware->ImageURL
+            );*/
+            $i++;
+        }
+        
+        // return the cookware to the "views/search_json.php" view so it can build valid JSON from the data
+        $data = array("json" => array("cookware" => $cookwareToReturn) );
+        $this->load->view('search_json', $data);
+    }
 }
 
 /* End of file search.php */
