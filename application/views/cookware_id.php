6e1a412590f82297d5d71d4b7432569a287d05cd
Jacob Fischer <jfischer@ubuntu-server.(none)>
2012-02-21 16:32:54 -0600

    Add the "Cookware View" so members with "SOMETHING View" can get their thing done

---
+++ application/views/cookware_id.php
@@ -0,0 +1,6 @@
+<h1>Cookware: <?=$cookware->Name?></h1>
+<ul>
+    <li>ID: <?=$cookware->ID?></li>
+    <li>Description: <?=$cookware->Description?></li>
+    <li>ImageURL: <?=$cookware->ImageURL?></li>
+</ul>
