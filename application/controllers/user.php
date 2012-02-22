74318439fb6fc94dd61ae09191f7de1c63006c61
Jacob V. Gardner <jacob.v.gardner@gmail.com>
2012-02-22 00:25:34 -0600

    Search by username.  Haha Jake Fischer.

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -2,16 +2,35 @@
 
 class User extends CI_Controller {
 
-  public function index($name)
+  public function index($id)
   { 
+    $this->db->select('*');
+    $this->db->from('Users');
+    $this->db->where('ID', $id );
+
     $this->template->load('error', array('title' => 'Cookware Not Found!', "message" => "The Cookware with id \"$name\" could not be found!") );
     // print $name;
   }
 
 
-  public function id($id)
+  public function name($name)
   {
-    $this->template->load('error', array('title' => 'Cookware Not Found!', "message" => "The Cookware with id \"$id\" could not be found!") );
+    $this->db->select('ID');
+    $this->db->from('Users');
+    $this->db->where('DisplayName', $name );
+
+    $query = $this->db->get();
+
+    if( $query->num_rows() == 1 )
+    {
+      index( $query->row(0) );
+      
+    } 
+    else
+    {
+      $this->template->load('error', array('title' => 'Username Not Found', "message" => "The username \"$name\" could not be found!") );
+    }
+    
     // print $id;
   }
 }
