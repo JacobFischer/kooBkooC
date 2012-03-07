2154a8fa5e130a6fc07a1b870200f7cb2b701b46
cwilliams <cjw5xd@mail.mst.edu>
2012-03-07 15:14:36 -0600

    more

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -113,7 +113,7 @@ class Search extends CI_Controller {
         
         // Execute database query
         if(strlen($target)) {
-            $query = $this->db->query("SELECT DISTINCT ID, Name, BaseUnitOfMeasure, Description, ImageURL FROM ((SELECT ID, Name, BaseUnitOfMeasure, Description, ImageURL, 0 AS SortOrder FROM Ingredients WHERE Name LIKE '{$target}%') UNION (SELECT ID, Name, BaseUnitOfMeasure, Description, ImageURL, 1 AS SortOrder FROM Ingredients WHERE Name LIKE '%{$target}%')) ORDER BY SortOrder, Name LIMIT {$this->SEARCH_TAG_PAGE_OFFSET} OFFSET {$page * $this->SEARCH_TAG_PAGE_OFFSET}");
+            //$query = $this->db->query("SELECT DISTINCT ID, Name, BaseUnitOfMeasure, Description, ImageURL FROM ((SELECT ID, Name, BaseUnitOfMeasure, Description, ImageURL, 0 AS SortOrder FROM Ingredients WHERE Name LIKE '{$target}%') UNION (SELECT ID, Name, BaseUnitOfMeasure, Description, ImageURL, 1 AS SortOrder FROM Ingredients WHERE Name LIKE '%{$target}%')) ORDER BY SortOrder, Name LIMIT {$this->SEARCH_TAG_PAGE_OFFSET} OFFSET {$page * $this->SEARCH_TAG_PAGE_OFFSET}");
         }
         else {
             $query = $this->db->query("SELECT DISTINCT ID, Name, BaseUnitOfMeasure, Description, ImageURL FROM Ingredients ORDER BY Name LIMIT {$this->SEARCH_TAG_PAGE_OFFSET} OFFSET {$page * $this->SEARCH_TAG_PAGE_OFFSET}");
