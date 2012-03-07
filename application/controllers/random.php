4c7fbee2ac4240537d206a18d796e701df9fa8e8
cwilliams <cjw5xd@mail.mst.edu>
2012-03-07 17:19:18 -0600

    slkdjfls

--- application/controllers/random.php
+++ application/controllers/random.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Random extends CI_Controller {
+class GetRandom extends CI_Controller {
     // ------------------------------------------------------------------------
     // Class variable declarations.
     // ------------------------------------------------------------------------
