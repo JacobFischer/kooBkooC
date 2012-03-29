149845a129c6838bd0c6c6f5fef9e5091d953313
cwilliams <cjw5xd@mail.mst.edu>
2012-03-29 15:45:01 -0500

    Cleaned up top.php; removed view __text__.php

--- application/controllers/top.php
+++ application/controllers/top.php
@@ -1,16 +1,17 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Top extends CI_Controller {
-    // ------------------------------------------------------------------------
-    // Class variable declarations.
-    // ------------------------------------------------------------------------
+    
+    // ========================================================================
+    // Class variable declarations
+    // ========================================================================
     
     // Value for SQL LIMIT when querying the database for top results.
-    //
-    // ########################################################################
-    // THE VALUES OF THIS VARIABLE MUST BE AN INTEGER!
-    // ########################################################################
-    protected $TOP_QUERY_GENERAL_LIMIT = 10;
+    protected $TOP_LIMIT = 10;
+    
+    // ========================================================================
+    // Class function definitions
+    // ========================================================================
     
     // ------------------------------------------------------------------------
     // Return all information on the top ingredients; top ingredients are those
@@ -22,24 +23,21 @@ class Top extends CI_Controller {
     public function ingredients()
     {
         // Create result array and empty sub-array
-        $result = array("json" => array("top_ingredients" => array()));
+        $result = array("json" => array("ingredients" => array()));
         
         // Execute database query
         $query = $this->db->query("SELECT ID, Name, BaseUnitOfMeasure, "
           ."Description, ImageURL FROM Ingredients WHERE ID IN (SELECT * FROM "
           ."(SELECT IngredientsID FROM RecipesIngredients GROUP BY RecipesID "
-          ."ORDER BY COUNT(RecipesID) DESC LIMIT "
-          .$this->TOP_QUERY_GENERAL_LIMIT.") AS subtable) ORDER BY Name");
+          ."ORDER BY COUNT(RecipesID) DESC LIMIT ".$this->TOP_LIMIT.") AS "
+          ."subtable) ORDER BY Name");
         
         // Append query records to result sub-array
         foreach($query->result() as $row) {
-            $result["json"]["top_ingredients"][] = $row;
+            $result["json"]["ingredients"][] = $row;
         }
         
         // Load result records into view for retrieval
         $this->load->view('search_json', $result);
     }
 }
-
-/* End of file top.php */
-/* Location: ./application/controllers/top.php */
