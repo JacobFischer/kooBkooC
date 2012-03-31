afdbcddbdd6a639b47b730ea206294c0dd68ab65
Jacob Fischer <jtf3m8@mst.edu>
2012-03-31 15:24:00 -0500

    Now our webstie is pretty! Also search.php is ugly as sin, I'm looking into refactoring it

--- application/views/template_default.php
+++ application/views/template_default.php
@@ -1,24 +1,59 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <?$this->load->helper('url');?>
-    <?foreach($styles as $style):?>  
-      <link rel="stylesheet" type="text/css" href="<?=base_url()?>/styles/<?=$style?>"></script>
-    <?endforeach;?>
-    <script type="text/javascript">
-        var base_url = "<?=base_url()?>";
-    </script>
-    <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
-    <?foreach($scripts as $script):?>  
-    <script type="text/javascript" src="<?=base_url()?>/scripts/<?=$script?>"></script>
-    <?endforeach;?>
-    <title>Social Reverse Cookbook</title>
-  <body>
-    <h1>KoobkooC</h1>
-    <div id="contents">
-        <?= $contents ?>
-    </div>
-    <div id="footer">It's Cookbook backwards. Trololololol</div>
-  </body>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>kooBkooC</title>
+        
+        <script type="text/javascript">
+          var base_url = "<?=base_url()?>";
+        </script>
+        
+        <!-- BEGIN: Style Sheet imports -->
+        <link href="<?=base_url()?>/styles/reset.css" rel='stylesheet' type='text/css'>
+        <link href="<?=base_url()?>/styles/style.css" rel='stylesheet' type='text/css'>
+        <?foreach($styles as $style):?>  
+        <link rel="stylesheet" type="text/css" href="<?=base_url()?>/styles/<?=$style?>"></script>
+        <?endforeach;?>
+        <!-- END: Style Sheet imports -->
+        
+        <!-- BEGIN: Javascript script imports -->
+        <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+        <?foreach($scripts as $script):?>  
+        <script type="text/javascript" src="<?=base_url()?>/scripts/<?=$script?>"></script>
+        <?endforeach;?>
+        <!-- END: Javascript script imports -->
+    </head>
+    <body>
+        <div id="template-title">
+            <a href="#" id="website-title">koo<span class="cap">B</span>koo<span class="cap">C</span></a>
+            <div id="profile-top">
+                <a href="#">Login</a> - <a href="#">Register</a>
+            </div>
+        </div>
+        <div id="wrapper-top">  
+            <header id="template-top">
+                <nav id="website-links">
+                    <ul>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Recipies</a>
+                        </li>
+                        <li>
+                            <a href="#">About Us</a>
+                        </li>
+                        <li>
+                            <a href="#">FAQ</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact Us</a>
+                        </li>
+                    <ul>
+                </nav>
+            </header>
+        </div>
+        <div id="template-main">
+            <?= $contents ?>
+        </div>
+    </body>
 </html>
