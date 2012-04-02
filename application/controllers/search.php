e97c3f3d5423b0aa528ee4bf77d98a8064a9cfa7
David <dmc8vb@mst.edu>
2012-04-02 01:55:36 -0500

    Updated Tag search for partial words/strings from user input

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -391,77 +391,60 @@ class Search extends CI_Controller {
     // END OF UNKNOWN
     // ########################################################################
 
-    /////////////////////////////////////////////////////////////////////////////////////////
-       // The basic JSON search function for Tags
-       // http://home.jacobfischer.me/USERNAME/cs397/index.php/search/tags/
-       public function tags()
-       {
-         // We want to return the $data object with a map key to json, and in the  json we are building the cookware
-         $data = array("json" => array("tags" => array() ) );
-        
-         // Build the SQL-ish query using CodeIgniters's Active Record
-         $this->db->select('*');
-         $this->db->from('Tags');
-        
-         // Actually execute the SQL Query on the database
-         $query = $this->db->get();
-        
-         // Iterate through each result in the query and build the cookware to  return
-         $i = 0;
-         foreach($query->result() as $tags)
-         {
-            $data['json']['tags'][$i] = $tags;
-            $i++;
-         }
-        
-        // return the tags to the "views/search_json.php" view so it can build  valid JSON from the data
-        $this->load->view('search_json', $data);
-      }
-      /////////////////////////////////////////////////////////////////////////////////////////
-
-
-
- /////////////////////////////////////////////////////////////////////////////////////////
-          // ------------------------------------------------------------------------
-    // Return all information on tags selected based on the target
-    // input; note that page refers to a perceived page number and not to the
-    // SQL OFFSET value. Use for general tag searching.
-    //
-    // http://home.jacobfischer.me/USERNAME/cs397/index.php/search/tag/_tagname_/_page#_
-    // ------------------------------------------------------------------------
-    
-    public function tag($target = "", $page = 1)
+    ///////////////////////////////////////////////////////////////////////////////////////
+    //Tag search for tags containing user entered text.
+    ///////////////////////////////////////////////////////////////////////////////////////
+    public function tags($searchVal = "", $page = 1)
     {
-        // Check invalid page
-        if($page < 1) {
-            $page = 1;
-        }
-        
-        // Create result array and empty sub-array
-        $result = array("json" => array("tag" => array()));
-        
-        // Prepare database query //////////////////////////////////////////////////////
+      if($page<1){$page=1;} //Validate Page Number
+      $returnVal=array( "json" => array( "tags" => array() ) );
+      $delimiter=$this->SEARCH_TAG_PAGE_OFFSET;
+      $offset=($page - 1)*$this->SEARCH_TAG_PAGE_OFFSET;
+      $searchVal=$this->tag_escape($searchVal);
+      /////////////////////////////////////////////////////////  
+      if( strlen($searchVal) )
+      {
         $this->db->select("ID, Name, Description");
-        $this->db->like("Name", $this->tag_escape($target), "after");
-        $this->db->order_by("CHAR_LENGTH(Name), Name");
-        
-        if(strlen($target)) {
-            $this->db->limit($this->SEARCH_TAG_GENERAL_LIMIT,
-              ($page - 1) * $this->SEARCH_TAG_PAGE_OFFSET);
-        }///////////////////////////////////////////////////////////////////////////////
-        
-        // Execute database query
-        $query = $this->db->get("Tags");
-        
-        // Append query records to result sub-array
-        foreach($query->result() as $row) {
-            $result["json"]["tag"][] = $row;
+        $this->db->like("Name", $searchVal, "after");
+        $this->db->order_by("Name");
+        $this->db->limit($delimiter, $offset);
+        $initialQuery=$this->db->get("Tags");
+        $this->db->flush_cache();
+        $this->db->select("ID, Name, Description");
+        $this->db->like("Name", $searchVal, "both");
+        $this->db->not_like("Name", $searchVal, "after");
+        $this->db->order_by("Name");
+        $this->db->limit($delimiter, $offset);
+        $secondaryQuery = $this->db->get("Tags");
+        ///////////////////////////////////////////////////////
+        foreach($initialQuery->result() as $result)
+        {
+          $returnVal["json"]["tags"][] = $result;
         }
-        
-        // Load result records into view for retrieval
-        $this->load->view('search_json', $result);
+        foreach($secondaryQuery->result() as $result)
+        {
+          $returnVal["json"]["tags"][] = $result;
+        }
+      }
+      /////////////////////////////////////////////////////////
+      else
+      {
+        $this->db->select("ID, Name, Description");
+        $this->db->order_by("Name");
+        $this->db->limit($delimiter, $offset);
+        $query = $this->db->get("Tags");
+        foreach($query->result() as $result)
+        {
+          $returnVal["json"]["tags"][] = $result;
+        }
+      }
+      ///////////////////////////////////////////////////////// 
+      $this->load->view( 'search_json', array_slice($returnVal, 0, $delimiter, TRUE) );
     }
-
+    ///////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+    
+    
 //>>>>>>> 2d41820e31e995b1b518e402049914b619b757bc
 }
 
