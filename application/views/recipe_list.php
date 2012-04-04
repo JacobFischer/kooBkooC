e62c2755e38ab98db9bb71f3cf38a92dc3cc501b
mwilson <mbw27f@mail.mst.edu>
2012-04-04 17:06:42 -0500

    alright tyrin again.  recipe_list and rcipe_page

---
+++ application/views/recipe_list.php
@@ -0,0 +1,7 @@
+<h1>Recipes Tagged &quot;<?=$tag->Name?>&quot;</h1>
+<ul>
+<?foreach($recipe->result() as $i){?>
+<?$j = $i->ID?>
+<li><a href="<?=base_url() . "index.php/recipe/id/$j"?>"> <?echo $i->Description?> </a> </li>
+<? } ?>
+</ul>
