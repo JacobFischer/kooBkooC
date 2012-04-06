96b64f26991aad42e8efd01de3701a0af87581e3
Jacob Gardner <jacob.v.gardner@gmail.com>
2012-04-05 19:27:14 -0500

    Downvoting, biatch

--- application/controllers/vote.php
+++ application/controllers/vote.php
@@ -68,6 +68,57 @@ class Vote extends CI_Controller {
         $this->template->load('error', array('title' => 'Recipe Invalid!', "message" => "The recipe you voted is not here.") );
         return;
       }
+
+
+      $this->load->library('session');
+
+      if(!$this->session->userdata('logged_in') || !$this->session->userdata('email'))
+      {
+        $this->template->load('error', array('title' => 'Must Be Logged In To Use This Function!', "message" => "You Must Be 18 or Older In Order To Do This.") );
+        return;
+      }
+      else
+      {
+        $this->db->select('*');
+        $this->db->from('Users');
+        $this->db->where('Email', $this->session->userdata('email'));
+
+        $query = $this->db->get();
+
+        if($query->num_rows() == 1)
+        {
+
+          $id = $query->row(0)->ID;
+          $data = array(
+            'UsersID' => $id,
+            'RecipesID' => $recipeID, 
+            'Direction' => -1,
+          );
+
+
+          $this->db->flush_cache();
+
+          $this->db->select('*');
+          $this->db->from('Votes');
+          $this->db->where('UsersID', $id);
+
+          $query = $this->db->get();
+          if($query->num_rows() == 0)
+          {
+            $this->db->set($data);
+            $this->db->insert('Votes');
+          } else
+          {
+
+            $this->db->where('UsersID', $id);
+            $this->db->update('Votes', $data);
+          }
+
+        }
+
+      }
+
+
     }
 
 }
