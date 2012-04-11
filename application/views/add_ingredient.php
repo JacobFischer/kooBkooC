c83b7534e8a117b3afb256f4bd0610a7a189f5cd
tpund <tapd4f@mst.edu>
2012-04-11 14:47:33 -0500

    added js stuff

--- application/views/add_ingredient.php
+++ application/views/add_ingredient.php
@@ -1,6 +1,6 @@
 <h1>Add an Ingredient</h1>
 <form action="<?=base_url() . "index.php/ingredients/submit/"?>" method="post">
-  Name: <input name="ingredient" type = "text"/><br/>
+  Name: <input name="ingredient" type = "text"/> <span id="guesser">Guess Ingredient</span><br/>
   Description: <input name="description" type = "text"/><br/>
   Base unit if measurement: <input name="measurement" type="text"/><br/>
   Image: <br/>
