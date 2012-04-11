f2a224409fe2d350ce6a3953609bf812e1c54358
tpund <tapd4f@mst.edu>
2012-04-11 17:24:30 -0500

    yup

--- application/views/add_tag.php
+++ application/views/add_tag.php
@@ -1,6 +1,6 @@
 <h1>Add a Tag!</h1>
 <form action="<?=base_url() . "index.php/tags/submit/"?>" method="post">
-  Name: <input name="tag_name" type = "text"/><br/>
+  Name: <input name="tag_name" type = "text"/><span id="guesser">Guess Tag</span><br/>
   Description: <input name="description" type = "text"/><br/>
   <input name="submit_button" type="submit" value="Submit Tag!"/>
 </form>
--- scripts/submit_guess.js
+++ scripts/submit_guess.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
   $("#guesser").click(function() {
     alert("Woop");
+    $.ajax
+    (
+      {
+        url: base_url(),
+        context: document.body,
+        success: function()
+        {
+          alert("WUT");
+        }
+      }
+    );
   });
 });
