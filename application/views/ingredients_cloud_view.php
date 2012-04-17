1e1c6d1418aaa46e9e43a1f839f9deabdb800cfc
mwilson <mbw27f@mail.mst.edu>
2012-04-17 18:08:41 -0500

    Got picture cloud working for ingredient view.

--- application/views/ingredients_cloud_view.php
+++ application/views/ingredients_cloud_view.php
@@ -1,8 +1,20 @@
 <h1>Ingredients</h1>
 
 <?foreach($ingredient->result() as $i):?>
-<?$j = $i->ID?>
-<a href="<?=base_url() . "index.php/ingredients/id/$j"?>" style="font-size:<?=($i->freq * $max_font)/$total?>pt;"><?=$i->Name?></a>
+<?$j = $i->ID;?>
+<?$z = (($i->freq * $max_font)/$total);
+	if($z > 400)
+	{
+	$z = 400;
+	}
+	
+	if($z < 10 )
+	{
+		$z = 10;
+	} 
+?>
+
+<a href="<?=base_url() . "index.php/ingredients/id/$j"?>" style="font-size:<?=($i->freq * $max_font)/$total?>pt;"><img src ="http://home.jacobfischer.me/cs397_uploads/ingredients/<?=$j?>.jpg" style="height: <?=$z * 4?>px; width: auto;"/></a>
 <? endforeach; ?>
 
 <br/><br/>
